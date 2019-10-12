2d6effb0a94d6e59bad10a7fa8f3e587d9c6ba5b
Sergey Matveev <darkus007@yandex.ru>
2019-10-12 18:23:54 +0300

    Section four rewritten using tiny-slider

--- src/js/script.js
+++ src/js/script.js
@@ -1,6 +1,6 @@
-$(document).ready(function(){ //* jquery библиотека $(document).ready(function() - когда документ готов (загружен) выполнить функцию */
-    $('.carousel__inner').slick({ //* запускаем слайдер */
-        speed: 1200,
+/* $(document).ready(function(){ // jquery библиотека $(document).ready(function() - когда документ готов (загружен) выполнить функцию 
+     $('.carousel__inner').slick({ // запускаем слайдер 
+         speed: 1200,
         prevArrow: '<button type="button" class="slick-prev"><img src="icons/left.svg"></button>',
         nextArrow: '<button type="button" class="slick-next"><img src="icons/right.svg"></button>',
         responsive: [{ // адаптация карусели под различные разрешения 
@@ -9,6 +9,46 @@ $(document).ready(function(){ //* jquery библиотека $(document).ready(
                 dots: true,
                 arrows: false
             }
-        }]
+        }] 
     }); 
-  });
+  }); */
+
+const slider = tns({
+    container: '.carousel__inner',
+    items: 1,
+    slideBy: 'page',
+    autoplay: false,
+    /* nav: false, */
+    navPosition: 'bottom',
+
+    controls: false,
+    /* controlsText: ['<img src="icons/left.svg">', '<img src="icons/right.svg">'] */
+    responsive: { /* адаптация под разные разрешения */
+        640: {
+            items: 1,
+            slideBy: 'page',
+            autoplay: false,
+        },
+        700: {
+            items: 1,
+            slideBy: 'page',
+            autoplay: false,
+        },
+        900: {
+            items: 1,
+            slideBy: 'page',
+            autoplay: false,
+        }
+    }
+});
+
+/* document.querySelector('.prev').onclick = function () {
+    slider.goTo('prev');
+}; */
+
+document.querySelector('.prev').addEventListener('click', function () {
+    slider.goTo('prev');
+});
+document.querySelector('.next').addEventListener('click', function () {
+    slider.goTo('next');
+});
